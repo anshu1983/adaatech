5e0e9722af47cdd205a6b1494600753439ea0a38
anshu1983 <71565162+anshu1983@users.noreply.github.com>
2021-01-21 00:10:02 +0000

    Add files via upload

---
+++ Hidden- -Auto-Incognito-Filter_v1.3/background.js
@@ -0,0 +1,96 @@
+// Github: https://github.com/pmsosa
+
+
+//Blacklist
+var blacklist = [ ];
+var KEY = "HIDD3N"
+
+
+//Add a new item to blacklist
+function addblacklist(item){
+  
+  blacklist.push(item.toLowerCase());
+  blacklist.sort();
+  toSave = {};
+  toSave[KEY] = JSON.stringify(blacklist);
+  chrome.storage.sync.set(toSave,function(){console.log("saved new case")});
+}
+
+//Remove item from blacklist
+function removeblacklist(item,callback){
+
+  blacklist.splice(blacklist.indexOf(item.toLowerCase()),1);
+  blacklist.sort();
+  
+  toSave = {};
+  toSave[KEY] = JSON.stringify(blacklist);
+  chrome.storage.sync.set(toSave,callback);
+}
+
+//Is a URL blacklisted
+function blacklisted(url){
+  for (var i = 0; i < blacklist.length; i++){
+    if (url.includes(blacklist[i])){
+      return true;
+    }
+  }
+  return false;
+}
+
+
+//Callback Function to Hide a new requested site!
+//Note: Chrome.tabs.get will throw a bunch of errors because when it pre-fetches websites, it does so from a tab that does not exist.
+function hide(info){
+
+  //Yes I know there is a race condition here, but I'm purpously "risking" it for now.
+
+  if (blacklisted(info.url)){
+    chrome.tabs.get(info.tabId, function(tab){
+      if (info.type=="main_frame" & tab != undefined){
+        
+        url = info.url;
+        console.debug(info);
+
+        chrome.tabs.remove(info.tabId);
+        chrome.windows.create({url: url , incognito: true});
+
+      } //Endif
+    }); //EndTabs
+    return {cancel: true};
+  } //EndBlacklist
+} //EndHide
+
+
+function cleanlist(){
+  var tempBlacklist = []
+  for (var i = 0; i < blacklist.length; i++){
+    if (blacklist[i].length > 2){
+      tempBlacklist.push(blacklist[i])
+    }
+  }
+  blacklist = tempBlacklist;
+}
+
+//Listener
+chrome.webRequest.onBeforeRequest.addListener(hide,{urls:["<all_urls>"]},["blocking"]);
+initBlacklist();
+
+//Initialize Blacklist
+function initBlacklist(){
+  chrome.storage.sync.get(KEY,
+    function(item){
+      console.log(item);
+      //Blacklist does not exist!
+      if (Object.keys(item).length === 0){
+        console.log("not found!");
+        blacklist = [ ];
+      }
+      //Blacklist was found!
+      else{
+        blacklist = JSON.parse(item[KEY]);
+        cleanlist();
+        console.log("found!")
+      } 
+    }
+  );
+}
---
+++ Hidden- -Auto-Incognito-Filter_v1.3/popup.js
@@ -0,0 +1,101 @@
+// Github: https://github.com/pmsosa
+
+//Add Item Button Action
+function additem(){
+	console.log("adding...");
+	newitem = document.getElementById("newitem").value;
+	if (newitem.length <= 2){
+		return
+	}
+	if (newitem.value != ""){
+		chrome.extension.getBackgroundPage().addblacklist(newitem);
+		fill_blacklist();
+		document.getElementById("newitem").value = "";
+	}
+	else{
+		newitem.focus();
+	}
+}
+
+//Remove Item Button Action
+function rmitem(){
+	console.log("rming...");
+	newitem = itemlist.value;
+	if (newitem != ""){
+		chrome.extension.getBackgroundPage().removeblacklist(newitem,fill_blacklist);
+		rmbtndisable()
+	}
+	else{
+		itemlist.focus();
+	}
+}
+
+//Remove Button Disabling
+function rmbtndisable(){
+	if (itemlist.value == ""){
+		btrm.disabled = true;
+	}
+	else{
+		btrm.disabled = false;
+	}
+}
+
+
+//Fills the Blacklist with the Filtered Keywords
+function fill_blacklist(){
+	itemlist.innerHTML = ""
+	bl = chrome.extension.getBackgroundPage().blacklist;
+	if (bl != []){
+		for (i = 0; i < bl.length; i++){
+			var opt = document.createElement("option");
+			opt.value = bl[i];
+			opt.innerHTML = bl[i];
+			itemlist.appendChild(opt);
+
+		}
+	}
+}
+
+
+//Show Instructions
+function showInstructions(){
+chrome.tabs.create({
+  url: chrome.extension.getURL("index.html")
+});
+}
+
+
+
+
+//Run this when you load!
+document.addEventListener('DOMContentLoaded', function () {
+	var btadd = document.getElementById("btadd");
+	var btrm  = document.getElementById("btrm");
+	var itemlist = document.getElementById("itemlist");
+	var instr = document.getElementById("instr");
+	
+
+	btadd.addEventListener('click',additem);
+	btrm.addEventListener('click',rmitem);
+	itemlist.addEventListener('change',rmbtndisable);
+	instr.addEventListener('click',showInstructions);
+	btrm.disabled = true;
+	fill_blacklist();
+
+
+	//Key Bindings to make the Extension a bit more user friendly
+	document.addEventListener("keydown", function(e){
+	    if(e.keyCode === 13 && document.activeElement.id == 'newitem') {
+	        additem();
+	    }
+
+	    if(e.keyCode === 8 || e.keyCode === 46) {
+	    	if (document.activeElement.id == 'itemlist' && btrm.disabled === false){
+	        	rmitem();
+	    	}
+	    }
+	});
+
+
+});
+
